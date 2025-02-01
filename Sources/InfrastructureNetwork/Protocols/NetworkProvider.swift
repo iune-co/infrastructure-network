a7734f74caf0f52a9d5725b6623a29224a4ee459
Ed <96289030+ed-iune@users.noreply.github.com>
2025-02-01 17:27:01 +0400

    Remove init requirement from NetworkProvider (#8)

--- Sources/InfrastructureNetwork/Protocols/NetworkProvider.swift
+++ Sources/InfrastructureNetwork/Protocols/NetworkProvider.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 public protocol NetworkProvider {
-        init(logger: NetworkLogger?, networkSession: NetworkSession)
         func request<
                 ResponseType: Decodable,
                 EndpointType: Endpoint
