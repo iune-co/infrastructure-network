86f2e6603feca774622aaf096fe2357a2278456e
ed-iune <ed@iune.co>
2025-04-18 22:43:52 +0400

    Add cache factory method

--- Sources/InfrastructureNetwork/Factory/NetworkProviderFactory.swift
+++ Sources/InfrastructureNetwork/Factory/NetworkProviderFactory.swift
@@ -2,9 +2,27 @@ import Foundation
 
 public struct NetworkProviderFactory {
         public static func createDefault() -> NetworkProvider {
-                NetworkProviderImplementation(
+                let cache = URLCache(
+                        memoryCapacity: 50 * 1024,
+                        diskCapacity: 200 * 1024 * 1024,
+                        diskPath: "urlcache"
+                )
+                let config = URLSessionConfiguration.default
+                config.urlCache = cache
+                config.requestCachePolicy = .useProtocolCachePolicy
+                return NetworkProviderImplementation(
                         logger: NetworkLoggerImplementation(),
-                        networkSession: URLSession.shared
+                        networkSession: URLSession(configuration: config)
+                )
+        }
+        
+        public static func createDefault(with cache: URLCache) -> NetworkProvider {
+                let config = URLSessionConfiguration.default
+                config.urlCache = cache
+                config.requestCachePolicy = .useProtocolCachePolicy
+                return NetworkProviderImplementation(
+                        logger: NetworkLoggerImplementation(),
+                        networkSession: URLSession(configuration: config)
                 )
         }
 
